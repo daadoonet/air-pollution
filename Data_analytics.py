1c504b43a8c0705c78bb58693083e1f3f3fb6b3a
daadoonet <daadoo.net@google.com>
2020-10-09 11:17:48 +0330

    :)

--- Data_analytics.py
+++ Data_analytics.py
@@ -18,9 +18,7 @@ def plot(data_list,title):
 
     plt.xlabel('Date')
     plt.ylabel('Pollution')
-    return plt.show()
+    plt.show()
 
-x = plot(aqi_data_list.tail(7),'last 7 days')
-y = plot(aqi_data_list.tail(30),'last 30 days')
-
-y
+plot(aqi_data_list.tail(7),'last 7 days')
+plot(aqi_data_list.tail(30),'last 30 days')
